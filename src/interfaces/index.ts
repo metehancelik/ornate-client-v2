0ef2632a20ada58047c72efb171f433a3aa92cf9
Talha Guven <86348456+guventalha@users.noreply.github.com>
2023-07-29 12:06:58 +0300

    services created

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,38 @@ export interface LoginDataInterface {
     offerupNick: string
     password: string
 }
+export interface IInquiry{
+    id?:string;
+    offerupNick?: string;
+    firstName?:string;
+    customerName?:string;
+    productName?:string;
+    to?:string;
+    region?:string;
+    isImportant?:boolean
+    notes?:string
+}
+export interface IUser{
+    id?:string;
+    offerupNick?: string;
+    firstName?:string;
+    lastName:string;
+    role:string;
+    commissionRate:number;
+    billingInfo:IBillingInfo
+}
+export interface IBillingInfo{
+    fullName: string,
+      email: string,
+      address: string,
+      iban: string,
+      swift: string,
+}
+export interface ICallReqQuery{
+   
+      customerName: string;
+      phone: string;
+      isCalled: 'pending' | 'in progress' | 'done';
+      salesmanNote: String,
+      callerNote: String,
+}
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,4 @@
+import { ICallReqQuery, IInquiry, IUser } from "../interfaces";
 import httpClient from "../utils/httpClient";
 // import { LoginDataInterface } from "../interfaces/";
 
@@ -8,80 +9,81 @@ class UserService {
         return await httpClient.get('/users/all')
 
     }
+   
     //get all inquiries
-  getAllInquiries(query, page) {
-    return this.request.get(`${API_URL}/api/v1/inquiries?offerupNick=${query.offerupNick}&firstName=${query.firstName}&customerName=${query.customerName}&productName=${query.productName}&page=${page}`);
+  getAllInquiries(query:IInquiry, page:number) {
+    return httpClient.get(`/inquiries?offerupNick=${query.offerupNick}&firstName=${query.firstName}&customerName=${query.customerName}&productName=${query.productName}&page=${page}`);
   }
 
   //update inquiry
-  updateInquiry(payload) {
-    return this.request.patch(`${API_URL}/api/v1/inquiry/${payload.id}`, payload);
+  updateInquiry(payload:IInquiry) {
+    return httpClient.patch(`/api/v1/inquiry/${payload.id}`, payload);
   }
   //gel all call requests
-  getAllCallReqs(query, page) {
-    return this.request.get(`${API_URL}/api/v1/calls?offerupNick=${query.offerupNick}&firstName=${query.firstName}&customerName=${query.customerName}&page=${page}`);
+  getAllCallReqs(query:{offerupNick:string,firstName:string,customerName:string}, page:number) {
+    return httpClient.get(`/api/v1/calls?offerupNick=${query.offerupNick}&firstName=${query.firstName}&customerName=${query.customerName}&page=${page}`);
   }
   //get all users
-  getAllUsers(query, page) {
-    return this.request.get(`${API_URL}/api/v1/users/all?q=${query}&page=${page}`);
+  getAllUsers(query:string, page:number) {
+    return httpClient.get(`/api/v1/users/all?q=${query}&page=${page}`);
   }
   //get all sales for admin
-  getAllSales(page) {
-    return this.request.get(`${API_URL}/api/v1/sales?page=${page}`);
+  getAllSales(page:number) {
+    return httpClient.get(`/api/v1/sales?page=${page}`);
   }
-  createSales(payload) {
-    return this.request.post(`${API_URL}/api/v1/sales`, payload);
-  }
+//   createSales(payload) {
+//     return httpClient.post(`/api/v1/sales`, payload);
+//   }
 
-  getCommissions(payload) {
-    return this.request.post(`${API_URL}/api/v1/commission`, payload);
-  }
-  createInvoice(payload) {
-    return this.request.post(`${API_URL}/api/v1/invoice`, payload);
+//   getCommissions(payload) {
+//     return httpClient.post(`/api/v1/commission`, payload);
+//   }
+//   createInvoice(payload) {
+//     return httpClient.post(`/api/v1/invoice`, payload);
 
-  }
-  creatUser(payload) {
+//   }
+  creatUser(payload:IUser) {
 
-    return this.request.post(`${API_URL}/api/v1/auth/register`, payload);
-
-  }
-  getCallReqById(id) {
-    return this.request.get(`${API_URL}/api/v1/calls/call/${id}`);
+    return httpClient.post(`/api/v1/auth/register`, payload);
 
   }
-  getUser(id) {
-    return this.request.get(`${API_URL}/api/v1/users/${id}`);
+  getCallReqById(id:string) {
+    return httpClient.get(`/api/v1/calls/call/${id}`);
+
   }
-  editUser(id, payload) {
-    return this.request.put(`${API_URL}/api/v1/users/${id}`, payload);
+  getUser(id:string) {
+    return httpClient.get(`/api/v1/users/${id}`);
+  }
+  editUser(id:string, payload:IUser) {
+    return httpClient.put(`/api/v1/users/${id}`, payload);
   }
 
 
   //-------------------USER----------------------//
   // create call request
-  createCallReq(payload) {
-    return this.request.post(`${API_URL}/api/v1/calls`, payload);
+  createCallReq(payload:ICallReqQuery) {
+    return httpClient.post(`/api/v1/calls`, payload);
   }
   //update call request
-  updateCallReq(payload) {
-
+  updateCallReq(id:number,payload:ICallReqQuery) {
+        return httpClient.put(`calls/${id}`,payload)
   }
   // get user's inquiries
   getUserInquiries() {
-    return this.request.get(`${API_URL}/api/v1/my-inquiries`);
+    return httpClient.get(`/api/v1/my-inquiries`);
   }
 
   // add user's inquiry
-  createInquiry(payload) {
-    return this.request.post(`${API_URL}/api/v1/inquiries`, payload);
+  createInquiry(payload:IInquiry) {
+    return httpClient.post(`/api/v1/inquiries`, payload);
   }
 
-  getSalesNotice(query, page) {
-    return this.request.get(`${API_URL}/api/v1/sales/uiSales?q=${query}&page=${page}`);
-  }
-  getMySales(query, page) {
-    return this.request.get(`${API_URL}/api/v1/sales/?q=${query}&page=${page}`);
-  }
+//   getSalesNotice(query, page) {
+//     return httpClient.get(`/api/v1/sales/uiSales?q=${query}&page=${page}`);
+//   }
+//   getMySales(query, page) {
+//     return httpClient.get(`/api/v1/sales/?q=${query}&page=${page}`);
+//   }
 }
 
 export default new UserService()
